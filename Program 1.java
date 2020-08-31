c6ed0d12b5e7be293efcdd78f5436def21b616eb
sandeshagarwal <sandeshagarwal10@gmail.com>
2020-08-31 17:42:14 +0530

    P1 - A basic tree program

---
+++ Program 1.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class Node
+{
+  int key;
+  Node left;
+  Node right;
+  Node(int k)
+  {
+    key = k;
+  }
+ }
+ 
+ public class Main{
+ public static void main(String args[])
+ {
+  Node root = new Node(10);
+  root.left = new Node(20);
+  root.right = new Node(30);
+  root.left.left = new Node(40);
+  System.out.println(root.key);
+ }
+}
+  
+   
